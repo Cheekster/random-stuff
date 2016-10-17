69024f8bd55482463c81581c561037b51f3b42fe
PeanutFIsh <peanutfish2@gmail.com>
2016-10-17 12:40:48 +0200

    Create flappytext.js

---
+++ flappytext.js
@@ -0,0 +1,18 @@
+var score = 0;
+function Flap() {
+var f = Math.floor((Math.random * 2)+1);
+if (f == 1) {
+document.getElementById("status").innerHTML = "You flapped";
+} 
+else {
+GemO();
+}
+}
+function DoNotFlap() {
+var f = Math.floor((Math.random * 2)+1);
+if (f == 1) {
+document.getElementById("status").innerHTML = "You not flapped";
+} else {
+GemO();
+}
+}
