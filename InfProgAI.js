fa20d13cf3c9a9b11d6b6fff8925c0b092d40844
PeanutFIsh <peanutfish2@gmail.com>
2017-01-24 13:37:42 +0100

    Update InfProgAI.js

--- InfProgAI.js
+++ InfProgAI.js
@@ -1,1 +1,11 @@
+var di = document.getElementById("console");
 
+//Function for boot up the Infinite Program Engine
+function startAI() {
+  di.innerHTML += "Engine Started";
+};
+
+//Function for end it
+function stopAI() {
+  di.innerHTML += "Engine Stopped";
+}
